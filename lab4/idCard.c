b96b7d25c81cc21c7d3ebfbedd75e5339dbb88e4
Bibek <bibekrider321@gmail.com>
2022-02-09 01:25:07 +0545

    made minor changes on idcard.c

--- lab4/idCard.c
+++ lab4/idCard.c
@@ -1,47 +1,41 @@
-// wap to design the identity card
+// that displays the information in the form of id
 
 # include <stdio.h>
 void main()
 {
-    unsigned long int mobNum, id,rollno;
-    char name[20]; 
-    char bloodGroup[20],dob[20];
-    int n = 1,i;
+    char name[20];
+    int studentId[20];
+    int dob[20];
+    long int mobileNo[20];
+    char bloodGroup[20];
 
-    // this is wihout using array
+    printf("Enter the name:\n");
+    scanf("%s",name);
+    printf("The entered name is %s\n",name);
 
-    printf("Enter your name:\n"); 
-    fgets(name,20,stdin); 
+    printf("Enter the student Id:\n");
+    scanf("%d",&studentId);
+    printf("The entered student is %d",studentId);
 
-    printf("Enter you dob:\n");
-    fgets(dob,20,stdin);
+    printf("Enter dob:\n");
+    scanf("%d",&dob);
+    printf("The dob is %d\n",dob);
 
-    // this line takes until it encounters new line
-    // scanf("%[^\n]*%c",&name); 
-    for(i=0; i<n; i++)
-    {
-        printf("Enter the blood group:\n");
-        scanf("%s",&bloodGroup[i]);
-    }
-    printf("Enter the mobile number:\n");
-    scanf("%lu",&mobNum);
+    printf("Enter mobileNo");
+    scanf("%lu",&mobileNo);
+    printf("The entered mobile no is %lu",mobileNo);
 
-    printf("Enter the rollno.\n");
-    scanf("%lu",&rollno);
-
-    /*
-    printf("your name is %s\n",name);
-    printf("your mobile number is %lu\n",mobNum);
-    printf("Your roll no is %lu\n",rollno);
-    printf("your blood group is: %s\n",bloodGroup);
-    */
-    printf("| \tAsian College Of Higher Studies\t " "|\n");
-    printf("|                                     " "|\n");
-    printf("|" "Name: %s                            " " |\n",name);
-    printf("|" "Rollno: %lu                          " "|\n",rollno);
-    printf("|" "Blood Group:                         " "|\n",bloodGroup);
-    printf("|" "DOB: %s                              " "|\n",dob);
-    printf("|" "mobile no: %lu                       " "|\n",mobNum);
+    printf("Enter the bloodGroup:\n");
+    scanf("%s",&bloodGroup);
+    printf("your blood group is %s\n",bloodGroup);
 
 
+    printf("Id card\n");
+    printf("|name :       %s........................|\n",name);
+    printf("|student Id:  %d........................|\n",studentId);
+    printf("|dob:         %d/.......................|\n",dob);
+    printf("|mobile no:   %lu.......................|\n",mobileNo);
+    printf("|blood group: %s........................|\n",bloodGroup);
+    
+    
 }
